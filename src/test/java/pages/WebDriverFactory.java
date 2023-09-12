e08fce0546bed3903a87213b399fb8e9c7f57c64
LidiaTe <131401640+LidiaTe@users.noreply.github.com>
2023-09-12 10:04:36 +0300

    Fixed bugs

--- src/test/java/pages/WebDriverFactory.java
+++ src/test/java/pages/WebDriverFactory.java
@@ -4,9 +4,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public abstract class WebDriverFactory {
+public class WebDriverFactory {
 
-   protected static WebDriver createWebDriver(String browserName){
+   public WebDriver createWebDriver(String browserName){
         switch (browserName){
             case "chrome":
                 return new ChromeDriver();
